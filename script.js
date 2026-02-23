4a7929ffe1e5425a998e3a95c061c9c417e60315
ashrafultusher13 <ashrafultusher703@gmail.com>
2026-02-24 00:10:19 +0600

    completed interview and rejected button functionality for all cards

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let intList = [];
 let rejList = [];
+let currentStatus = "all";
 
 let totalCount = document.getElementById("totalcount");
 let intCount = document.getElementById("intercount");
@@ -21,6 +22,7 @@ function calculateCount() {
 calculateCount();
 
 function toggleStyle(id) {
+  currentStatus = id;
   // added bg-white for all
   allBtn.classList.add("bg-white", "text-[#64748B]");
   intBtn.classList.add("bg-white", "text-[#64748B]");
@@ -39,11 +41,23 @@ function toggleStyle(id) {
   if (id == "int-btn") {
     allCards.classList.add("hidden");
     filterSec.classList.remove("hidden");
+    putInt();
   } else if (id == "all-btn") {
     allCards.classList.remove("hidden");
     filterSec.classList.add("hidden");
+  } else if ((id = "rej-btn")) {
+    allCards.classList.add("hidden");
+    filterSec.classList.remove("hidden");
+    putRej();
   }
 }
+
+mainCon.addEventListener("click", function(event) {
+  
+  const parentNode = event.target.parentNode.parentNode;
+  event.target
+  
+})
 
 mainCon.addEventListener("click", function (event) {
   if (event.target.classList.contains("int-card-btn")) {
@@ -70,8 +84,45 @@ mainCon.addEventListener("click", function (event) {
     if (!companyExist) {
       intList.push(cardInfo);
     }
+    rejList = rejList.filter(
+      (item) => item.companyName != cardInfo.companyName,
+    );
     calculateCount();
-    putInt();
+    if (currentStatus == "rej-btn") {
+      putRej();
+    }
+  } else if (event.target.classList.contains("rej-card-btn")) {
+    const parentNode = event.target.parentNode.parentNode;
+    const companyName = parentNode.querySelector(".company-name").innerText;
+    const companyPost = parentNode.querySelector(".company-post").innerText;
+    const postDetails = parentNode.querySelector(".post-details").innerText;
+    const status = parentNode.querySelector(".state").innerText;
+    const workNote = parentNode.querySelector(".post-work").innerText;
+
+    parentNode.querySelector(".state").innerText = "REJECTED";
+
+    const cardInfo = {
+      companyName,
+      companyPost,
+      postDetails,
+      status: "REJECTED",
+      workNote,
+    };
+    const companyExist = rejList.find(
+      (item) => item.companyName == cardInfo.companyName,
+    );
+
+    if (!companyExist) {
+      rejList.push(cardInfo);
+    }
+    intList = intList.filter(
+      (item) => item.companyName != cardInfo.companyName,
+    );
+
+    if (currentStatus == "int-btn") {
+      putInt();
+    }
+    calculateCount();
   }
 });
 
@@ -79,7 +130,6 @@ function putInt() {
   filterSec.innerHTML = "";
 
   for (let int of intList) {
-    console.log(int);
     let div = document.createElement("div");
     div.className = "bg-white rounded-lg flex justify-between items-baseline";
     div.innerHTML = `
@@ -93,6 +143,49 @@ function putInt() {
               Remote • Full-time • $130,000 - $175,000
             </p>
             <p class="state text-[#002C5C]">${int.status}</p>
+            <p class="post-work text-[#323B49] mt-4">
+              Build cross-platform mobile applications using React Native. Work
+              on products used by millions of users worldwide.
+            </p>
+            <!-- Buttons -->
+            <div class="flex gap-2">
+              <button class="int-card-btn btn btn-outline btn-success">
+                Interview
+              </button>
+              <button class="rej-card-btn btn btn-outline btn-secondary">
+                Rejected
+              </button>
+            </div>
+          </div>
+
+          <!-- 2nd part card -->
+          <div class="mr-5">
+            <button class="text-[#64748B] btn btn-circle">
+              <i class="fa-regular fa-trash-can"></i>
+            </button>
+          </div>
+    `;
+    filterSec.appendChild(div);
+  }
+}
+
+function putRej() {
+  filterSec.innerHTML = "";
+
+  for (let rej of rejList) {
+    let div = document.createElement("div");
+    div.className = "bg-white rounded-lg flex justify-between items-baseline";
+    div.innerHTML = `
+          <!-- 1st part card -->
+          <div class="space-y-5 p-6">
+            <h2 class="company-name text-[#002C5C] font-bold">
+              ${rej.companyName}
+            </h2>
+            <p class="company-post text-[#64748B]">React Native Developer</p>
+            <p class="post-details text-[#64748B]">
+              Remote • Full-time • $130,000 - $175,000
+            </p>
+            <p class="state text-[#002C5C]">${rej.status}</p>
             <p class="post-work text-[#323B49] mt-4">
               Build cross-platform mobile applications using React Native. Work
               on products used by millions of users worldwide.
